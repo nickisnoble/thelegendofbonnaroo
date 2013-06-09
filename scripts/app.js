f81928472aed47895227254a1260e06fdaa1f376
Bryan Joseph <bryanjos@gmail.com>
2013-06-09 13:35:45 -0500

    More app.js changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 Parse.initialize('925qu3OQV7NK7DksVYwJSIdVvr8x5ZtLzEU5jg1i', 'yHldFlVEPsBCi6VY7WQbqEaGzuOun8ziZRHj4tvY');
 var currentUser = Parse.User.current();
+var maxLevel = 5;
 
 
 $(document).ready(function(){
@@ -14,42 +15,67 @@ $(document).ready(function(){
 
     $('#sign-out').click(function(){
         Parse.User.logOut();
-        window.location = '/';
+        window.location = '/login';
+    });
+
+    $('#sign-up').click(function(){
+        $('#sign-up').attr("disabled", "disabled");
+        signUp(
+            $('#username').text(),
+            $('#password').text(),
+            $('#email').text(),
+            {
+                success: function(user) {
+                    window.location = '/current.html';
+                    $('#sign-up').removeAttr("disabled");
+                },
+                error: function(user, error) {
+                    alert(error.message);
+                    $('#sign-up').removeAttr("disabled");
+                }
+            }
+        )
+    });
+
+    $('#login').click(function(){
+        $('#login').attr("disabled", "disabled");
+        login(
+            $('#username').text(),
+            $('#password').text(),
+            {
+                success: function(user) {
+                    window.location = '/current.html';
+                    $('#login').removeAttr("disabled");
+                },
+                error: function(user, error) {
+                    alert(error.message);
+                    $('#login').removeAttr("disabled");
+                }
+            }
+        )
     });
 });
 
-function isLoggedIn(){
-    return currentUser != null;
-}
-
-function signUp(username, password, email){
+function signUp(username, password, email, callback){
     var user = new Parse.User();
     user.set('username', username);
     user.set('password', password);
     user.set('email', email);
-
-    user.signUp(null, {
-        success: function(user) {
-            window.location = '/';
-        },
-        error: function(user, error) {
-            alert(error.message);
-        }
-    });
+    user.set('level', 0);
+    user.signUp(null, callback);
 }
 
-
-function login(username, password){
-    Parse.User.logIn(username, password, {
-        success: function(user) {
-            window.location = '/';
-        },
-        error: function(user, error) {
-            alert(error.message);
-        }
-    });
+function login(username, password, callback){
+    Parse.User.logIn(username, password, callback);
 }
 
+function levelUp(){
+    if(currentUser){
+        currentUser.set('level', currentUser.get('level') + 1);
+        currentUser.save();
+        window.location = '/current.html';
+    }
+}
 
 function getUserByUsername(username){
     var query = new Parse.Query(Parse.User);
